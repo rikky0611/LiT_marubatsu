59dde0abffa639f80bf4bdf6c787c7f1b2c24fb0
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2016-02-24 13:49:55 +0900

    selectorで指定するメソッドが引数をとるので : を追加

--- marubatsu/Board.swift
+++ marubatsu/Board.swift
@@ -7,3 +7,53 @@
 //
 
 import Foundation
+
+class Board{
+    //状態
+    enum Status{
+        case Blank
+        case First
+        case Second
+    }
+    
+    let lines = [
+        [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
+    ]
+    
+    var statuses = Array(count:9, repeatedValue:Status.Blank)
+    
+    var turn = 0
+    
+    func currentPlayer() ->Status{
+        if turn % 2 == 0{
+            return Status.First
+        }
+        else {
+            return Status.Second
+        }
+    }
+    
+    func canPut(i:Int) -> Bool{
+        if winner() != .Blank{
+            return false
+        }
+        
+        return statuses[i] == .Blank
+    }
+    
+    func put(i:Int){
+        statuses[i] = currentPlayer()
+        turn++
+    }
+    
+    func winner() -> Status{
+        for line in lines{
+            let l0 = line[0],l1 = line[1], l2 = line[2]
+            if statuses[l0] != .Blank && statuses[l0] == statuses[l1] && statuses[l1] == statuses[l2]{
+                return statuses[l0]
+            }
+        }
+        return .Blank
+    }
+}
+
--- marubatsu/ViewController.swift
+++ marubatsu/ViewController.swift
@@ -9,17 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var board = Board()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let screenWidth = UIScreen.mainScreen().bounds.size.width
+        let screenHeight = UIScreen.mainScreen().bounds.size.height
+        
+        let boardSize = CGSizeMake(screenWidth,screenHeight)
+        let boardOrigin = CGPointMake(0,(screenHeight - boardSize.height)/2)
+        
+        let boardView = UIView()
+        boardView.frame.origin = boardOrigin
+        boardView.frame.size = boardSize
+        self.view.addSubview(boardView)
+        
+        let btnSize = boardSize.width/3
+        
+        for var y = 0; y<3; y++ {
+            for var x = 0;x<3;x++ {
+                let btn = UIButton(frame: CGRectMake(btnSize * CGFloat(x),btnSize * CGFloat(y),btnSize, btnSize))
+                btn.layer.borderWidth = 2.0
+                btn.layer.borderColor = UIColor.grayColor().CGColor
+                boardView.addSubview(btn)
+                
+                btn.tag = y*3 + x
+                btn.addTarget(self, action:Selector("onBtnClick:") , forControlEvents: .TouchUpInside)
+                
+            }
+        }
     }
-
+    
+    func onBtnClick(btn : UIButton){
+        if board.canPut(btn.tag) != true{
+            return
+        }
+        btn.setTitle("\(board.currentPlayer())", forState: .Normal)
+        board.put(btn.tag)
+        
+        let winner = board.winner()
+        if winner != .Blank{
+            NSLog("\(winner)の勝ち！")
+            return
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
